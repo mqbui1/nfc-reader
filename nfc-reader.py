00600f581f7258477a3be5f6a406d8a5936042be
Marc Bui <mqbui1@gmail.com>
2023-03-11 23:34:30 -0800

    add book audio functionality

--- nfc-reader.py
+++ nfc-reader.py
@@ -10,17 +10,32 @@ import glob
 import random
 import time
 from pathlib import Path
+import codecs
 
 lastcard = 0
 currentcard = 1
 CID = 0
 introStarted = False
+isBook = False
+isReading = False
 songs=[]
 
-# Function to play MP3
-def playfile(mixer, song, volume):
+# Function to play main MP3
+def playmainfile(mixer, cardidentifier, volume):
     if mixer.music.get_busy():
-        mixer.music.stop()
+        mixer.music.stop()    
+    # Randomize audio from folder
+    directory = glob.glob('/home/pi/'+cardidentifier+'-*')
+    songlist = os.listdir(directory[0])
+    selected_song=random.choice(songlist)
+    mixer.music.load(directory[0]+'/'+selected_song)
+    mixer.music.set_volume(volume)
+    mixer.music.play()
+    
+# Function to play intro MP3
+def playintrofile(mixer, song, volume):
+    if mixer.music.get_busy():
+        mixer.music.stop()    
     mixer.music.load(song)
     mixer.music.set_volume(volume)
     mixer.music.play()
@@ -81,7 +96,10 @@ while True:
     # Try again if no card is available.
     if uid is None:
         print('Waiting for RFID/NFC card...')
-        stopfile()
+        if isBook == False:
+            print('stopping during waiting period')
+            stopfile()
+            #isBook = False
         lastcard = 999999999
         currentcard = 999999998
         introStarted = False
@@ -91,32 +109,43 @@ while True:
     # print("Character ID:", pn532.mifare_classic_read_block(21))
     
     try:
+        # Read UID from bytearray
         uid_str = binascii.hexlify(bytearray(pn532.read_passive_target(timeout=0.5)))
         cardidentifier= uid_str.decode()
-
-        #charid_str = binascii.hexlify(bytearray(pn532.mifare_classic_read_block(21)))
-        #cardidentifier= charid_str.decode()
+        #print(cardidentifier)
+        #time.sleep(99999)
         
-        # Sleep to get character ID
-        #print('cardidentifier: ' + cardidentifier)
-        #time.sleep(999999)
+        # Need to add check for mifare classic vs something else
         
-        # Play intro
-        if introStarted != True:
-            playfile(mixer, 'select_sound.mp3', 0.5)
+        # Check if NFC tag is a book based on value stored in block 6
+        readblock = bytearray(pn532.mifare_classic_read_block(6))
+        
+        # Check if the 'book' text exists in the 5th bit
+        if str(readblock[:5]) == "bytearray(b'nbook')":
+            isBook = True
+            #bookcheck = readblock[:5].decode('utf-8')       
+        # If NFC read is a book and reading is in progress, stop audio
+        if isBook == True and isplaying() == True:
+            print('stop reading book')
+            stopfile()
+            isBook = False
+            time.sleep(2)
+        # Else if NFC read is a book and reading is not in progress, start audio
+        elif isBook == True and isplaying() != True:
+            print('start reading')
+            playmainfile(mixer, cardidentifier, 0.5)
+            time.sleep(2)
+        # Else if NFC read is not a book
+        elif isBook == False and introStarted != True:
+            playintrofile(mixer, 'select_sound.mp3', 0.5)
             time.sleep(1)
             my_file = Path('/home/pi/'+cardidentifier+'.mp3')
             if my_file.is_file():
-                playfile(mixer, '/home/pi/'+cardidentifier+'.mp3', 0.5) 
+                playintrofile(mixer, '/home/pi/'+cardidentifier+'.mp3', 0.5) 
             if isplaying() == True:
                 introStarted = True
         # Intro complete, play main song
-        elif isplaying() != True:
-            # Randomize character song
-            directory = glob.glob('/home/pi/'+cardidentifier+'-*')
-            songlist = os.listdir(directory[0])
-            selected_song=random.choice(songlist)
-            # Play randomized song
-            playfile(mixer, directory[0]+'/'+selected_song, 0.5)
+        elif isBook == False and isplaying() != True:
+            playmainfile(mixer, cardidentifier, 0.5)
     except:
         pass
